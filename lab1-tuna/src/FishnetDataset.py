b65f2bcd0741e98f8289c1915974629095e68e38
Stepan Ponomarev <stponomarevw@gmail.com>
2022-09-19 16:09:38 +0300

    WIP: finetune FishnetDataset

--- lab1-tuna/src/FishnetDataset.py
+++ lab1-tuna/src/FishnetDataset.py
@@ -1,13 +1,14 @@
 import os.path
 
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
 
 class FishnetDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None):
-        self.img_labels = pd.read_csv
+        self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
 
@@ -15,9 +16,20 @@ class FishnetDataset(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0] + ".jpg")
         image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        x_min = self.img_labels.iloc[idx, 2]
+        x_max = self.img_labels.iloc[idx, 3]
+        y_min = self.img_labels.iloc[idx, 4]
+        y_max = self.img_labels.iloc[idx, 5]
+        box = torch.as_tensor([[x_min, y_min, x_max, y_max]], dtype=torch.float32)
+        label = self.img_labels.iloc[idx, 7]
+        area = (box[:, 3] - box[:, 1]) * (box[:, 2] - box[:, 0])
+
+        target = {}
+        target["box"] = box
+        target["label"] = label
+        target["area"] = area
         if self.transform:
             image = self.transform(image)
 
--- lab1-tuna/src/main.py
+++ lab1-tuna/src/main.py
@@ -1,24 +1,23 @@
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from FishnetDataset import FishnetDataset
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter  # TensorBoard support
-from torchvision import datasets, transforms
+from torchvision import models
 
 tb = SummaryWriter(comment="Run simple cnn on mnist")
 
 # Device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")  # My GeForce 660 isn't supported because it's too old
 print(device)
 
-train_data = datasets.MNIST(
-    root="data",
-    train=True,
-    transform=transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()]),
-    download=True,
-)
-test_data = datasets.MNIST(
-    root="data",
-    train=False,
-    transform=transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()]),
+train_data = FishnetDataset(
+    annotations_file="../resources/labels/foid_labels_bbox_v012.csv",
+    img_dir="../resources/images",
+    transform=None,
 )
 
 figure = plt.figure(figsize=(10, 8))
@@ -26,12 +25,19 @@ cols, rows = 5, 5
 for i in range(1, cols * rows + 1):
     sample_idx = torch.randint(len(train_data), size=(1,)).item()
     img, label = train_data[sample_idx]
+    print("label={}".format(label))
     figure.add_subplot(rows, cols, i)
     plt.title(label)
     plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
+    plt.imshow(img.permute(1, 2, 0))
 plt.show()
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1)
+train_loader = DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1)
 
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=True, num_workers=1)
+test_loader = DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1)
+
+cnn = models.alexnet(num_classes=10).to(device)
+loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam(cnn.parameters(), lr=0.0015)
+
+print(cnn)
